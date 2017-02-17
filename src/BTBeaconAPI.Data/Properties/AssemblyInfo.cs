b63d89a07306cf7375b04207e2a62d1bbcb54bb5
Petteri Suominen <pspfolio@gmail.com>
2017-02-17 18:09:12 +0200

    basic project structure

---
+++ src/BTBeaconAPI.Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BTBeaconAPI.Data")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2cf48b59-bfa4-4ff0-882b-3ec899e0c3c3")]
---
+++ src/BTBeaconAPI/Controllers/BTBeaconController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BTBeaconAPI.Controllers
+{
+	[Route("api/beacon")]
+	public class BTBeaconController : Controller
+	{
+		[HttpGet("")]
+		public IActionResult GetAsync()
+		{
+			return Ok(new { Name = "BT", Message = "Jou!" });
+		}
+	}
+}
