317229e4f89090d66e88fbda756d860aade9d1fa
Petteri Suominen <pspfolio@gmail.com>
2017-02-17 17:35:21 +0200

    first commit

---
+++ src/BTBeaconAPI/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace BTBeaconAPI
+{
+	public class Startup
+	{
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+			if (env.IsEnvironment("Development"))
+			{
+				// This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+				builder.AddApplicationInsightsSettings(developerMode: true);
+			}
+
+			builder.AddEnvironmentVariables();
+			_config = builder.Build();
+		}
+
+		IConfigurationRoot _config;
+
+		// This method gets called by the runtime. Use this method to add services to the container
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddSingleton(_config);
+			// Add framework services.
+			services.AddApplicationInsightsTelemetry(_config);
+
+			services.AddMvc();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole(_config.GetSection("Logging"));
+			loggerFactory.AddDebug();
+
+			app.UseApplicationInsightsRequestTelemetry();
+
+			app.UseApplicationInsightsExceptionTelemetry();
+
+			app.UseMvc();
+		}
+	}
+}
